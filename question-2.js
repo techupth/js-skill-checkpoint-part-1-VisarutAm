76c77269e06557115912393f7e828ea3dda9b426
Ampare <visarut.thk@gmail.com>
2024-05-22 15:35:16 +0700

    JS-Skill-Checkpoint

--- question-2.js
+++ question-2.js
@@ -4,3 +4,15 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+inventory[0].quantity = 200;
+inventory.push({ name: "Orange", price: 20, quantity: 300 });
+// console.log(inventory)
+let totalPrice = 0;
+
+for (let item of inventory) {
+  // console.log(item.price)
+  // console.log(item.quantity)
+  totalPrice = totalPrice + item.price * item.quantity;
+}
+
+console.log("มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก " + totalPrice + " บาท");
--- question-4.js
+++ question-4.js
@@ -5,3 +5,25 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+// console.log(inventory[1].name)
+let minQuantity = inventory[0].quantity;
+// let nameMin = inventory[1].name;
+let nameMin = "";
+
+for (let item of inventory) {
+  // console.log(item.name)
+  // console.log(item.quantity);
+  if (item.quantity < minQuantity) {
+    minQuantity = item.quantity;
+    nameMin = item.name;
+  }
+}
+// console.log(nameMin);
+console.log(
+  "สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ " +
+    nameMin +
+    " ซึ่งมี " +
+    minQuantity +
+    " ชิ้น"
+);
+// console.log(`สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${nameMin} ซึ่งมี ${minQuantity} ชิ้น`)
